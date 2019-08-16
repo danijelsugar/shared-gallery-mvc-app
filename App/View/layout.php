8a20c21f24e70d51867f6b058700290410b28102
Danijel Šugar <danijel.sugar@outlook.com>
2019-08-16 18:09:36 +0200

    In base layout added navbar links leading to home page and login page, made login and registration pages

--- App/View/layout.php
+++ App/View/layout.php
@@ -6,19 +6,24 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+    
     <nav>
         <ul class="nav nav-tabs justify-content-center">
             <li class="nav-item">
-                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="<?php echo App::config('url'); ?>">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Login</a>
+                <a class="nav-link" href="<?php echo App::config('url') ?>login">Login</a>
             </li>
         </ul>
     </nav>
         
     <div class="container">
-        <?php echo $content; ?>
+        
+        <div class="row">
+            <?php echo $content; ?>
+        </div>
+        
     </div>
 
 
---
+++ App/View/login.php
@@ -0,0 +1,21 @@
+<div class="card col-md-6 offset-3">
+  <div class="card-body">
+        <form action="" method="post">
+            <h3 class="text-center text-info">Login</h3>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" class="form-control">
+            </div>
+            <div class="form-group">
+                <input type="submit" name="login" class="btn btn-info btn-md" value="Login">
+            </div>
+            <div class="form-group">
+                <p>Don't have an accout make one <a href="<?php echo App::config('url') ?>registration">here</a></p>
+            </div>
+        </form>
+  </div>
+</div>
