e2b504a664e383f120639b017d1d1d1e3f0e9cf8
Danijel Šugar <danijel.sugar@outlook.com>
2019-08-18 19:05:12 +0200

    Implemented user registration

--- App/Controller/RegistrationController.php
+++ App/Controller/RegistrationController.php
@@ -21,7 +21,48 @@ class RegistrationController
 
         if ($username === '') {
             $valid = false;
-            
+            Session::getInstance()->addMessage('Username required', 'warning');
         }
+
+        if ($email === '') {
+            $valid = false;
+            Session::getInstance()->addMessage('Email required', 'warning');
+        }
+
+        if ($password === '') {
+            $valid = false;
+            Session::getInstance()->addMessage('Password required', 'warning');
+        }
+
+        if ($rpassword === '') {
+            Session::getInstance()->addMessage('Repeat password required', 'warning');
+        }
+
+        if ($rpassword !== $password) {
+            $valid = false;
+            Session::getInstance()->addMessage('Passwords not matching', 'warning');
+        }
+
+        if ($valid) {
+            $db = Db::connect();
+            $stmt = $db->prepare('insert into user (firstname,lastname,username,email,password) 
+                values (:firstname,:lastname,:username,:email,:password)');
+            $stmt->bindValue('firstname', $firstName);
+            $stmt->bindValue('lastname', $lastName);
+            $stmt->bindValue('username', $username);
+            $stmt->bindValue('email', $email);
+            $stmt->bindValue('password', password_hash($password, PASSWORD_BCRYPT));
+            if ($stmt->execute()) {
+                Session::getInstance()->addMessage('You registered successfuly', 'success');
+            } else {
+                Session::getInstance()->addMessage('Something went wrong try again', 'info');
+            }
+            header("Location: " . App::config('url') . 'home');
+        } else {
+            header("Location: " . App::config('url') . 'registration');
+        }
+
+        
+        
     }
 }
