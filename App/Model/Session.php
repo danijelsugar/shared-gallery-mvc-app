e1d445dd4d670e9536dc8f1a288707f893568e03
Danijel Šugar <danijel.sugar@outlook.com>
2019-08-18 19:07:37 +0200

    Implemented flash messages

--- App/Model/Session.php
+++ App/Model/Session.php
@@ -18,6 +18,22 @@ class Session
 
     public function addMessage($message, $type = 'success')
     {
-        
+        if (!isset($_SESSION['flashMessage'])) {
+            $_SESSION['flashMessage'] = [];
+        }
+        $_SESSION['flashMessage'][] = [
+            'body' => $message,
+            'type' => $type
+        ];
+    }
+
+    public function getMessage()
+    {
+        if (isset($_SESSION['flashMessage'])) {
+            $message = $_SESSION['flashMessage'];
+            unset($_SESSION['flashMessage']);
+            return $message;
+            
+        }
     }
 }
