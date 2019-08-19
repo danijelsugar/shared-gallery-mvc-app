3b310c04b2bc08e7f422fb0447e44fcc43bfcc4d
Danijel Šugar <danijel.sugar@outlook.com>
2019-08-19 19:07:07 +0200

    Login and logout

--- App/Controller/LoginController.php
+++ App/Controller/LoginController.php
@@ -7,4 +7,54 @@ class LoginController
         $view = new View();
         $view->render('login');
     }
+
+    public function authorize()
+    {
+        $email = isset($_POST['email']) ? trim($_POST['email']) : '';
+        $password = isset($_POST['password']) ?  trim($_POST['password']) : '';
+        $valid = true;
+
+        if ($email === '') {
+            $valid = false;
+            Session::getInstance()->addMessage('Email required', 'warning');
+        }
+
+        if ($password === '') {
+            $valid = false;
+            Session::getInstance()->addMessage('Password required', 'warning');
+        }
+
+        if ($valid) {
+            $db = Db::connect();
+            $stmt = $db->prepare('select * from user where email=:email');
+            $stmt->bindValue('email', $email);
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                $user = $stmt->fetch();
+                
+                if (password_verify($password, $user->password)) {
+                    unset($user->password);
+                    Session::getInstance()->login($user);
+                    Session::getInstance()->addMessage('You have successfully logged in', 'success');
+                    header('Location: ' . App::config('url'));
+                } else {
+                    Session::getInstance()->addMessage('Incorrect combination of email/password', 'warning');
+                    $this->index();
+                }
+            } else {
+                Session::getInstance()->addMessage('Wrong mail', 'warning');
+                $this->index();
+            }
+        } else {
+            header('Location: ' . App::config('url') . 'login');
+        }
+    }
+
+    public function logout()
+    {
+        Session::getInstance()->logout();
+        Session::getInstance()->addMessage('You have successfully logged out', 'success');
+        header('Location: ' . App::config('url'));
+    }
 }
